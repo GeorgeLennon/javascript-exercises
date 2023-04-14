e3711f94fb6bcb61d227fc8ba5c9900609bd01cd
GeorgeLennon <130295939+GeorgeLennon@users.noreply.github.com>
2023-04-14 09:48:52 +0100

    solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(string==''){
+        return '';
+    }
+    switch(num){
+        case 1:
+            return string.repeat(1);
+        case (num>9):
+            return string.repeat(num);
+        case (num<0):
+            return 'ERROR';
+        case 0:
+            return '';
+        default:
+            if(num<0) return "ERROR";
+            return string.repeat(num);
+            
+    }
+    
 };
 
 // Do not edit below this line
