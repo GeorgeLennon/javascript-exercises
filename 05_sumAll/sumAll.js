7de808a3e723523e6d8c436384f893119fb7e950
GeorgeLennon <130295939+GeorgeLennon@users.noreply.github.com>
2023-04-14 11:17:35 +0100

    solved all but string

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    let sum=0;
+    if(num1<0||num2<0){
+        return "ERROR";
+    }
+     else if(num1<num2){
+        for(let i=num1;i<=num2;++i){
+           sum+=i;
+        }
+        return sum;
+     }else if(num1>num2){
+        for(let i=num1;i>=num2;--i){
+            sum+=i;
+        }
+        return sum;
+     }else if(typeof(num2)==String){
+        return "ERROR";
+     }else if(typeof(num1)!=Number||typeof(num2)!=Number){
+        return "ERROR"
+     }
+     else{
+        return "ERROR";
+     }
 };
 
 // Do not edit below this line
