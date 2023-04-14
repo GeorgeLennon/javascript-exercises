d8eba9b7e4c9a36a308ad97e9821a2259891e04f
GeorgeLennon <130295939+GeorgeLennon@users.noreply.github.com>
2023-04-14 10:15:19 +0100

    solved reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    //let arrofstr=string.split('');//splits string into substrings which fit into array indexes, seperated by seperators
+    //suych as spaces or commas
+    if(string==''){
+        return '';
+    }else{
+        
+        
+       
+        return  string.split('').reverse().join('');
+    }
 };
 
 // Do not edit below this line
